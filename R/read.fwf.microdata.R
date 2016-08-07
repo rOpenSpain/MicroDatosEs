6885a0a8c35f5b597c54383680aeae7f55f5ad25
Carlos J. Gil Bellosta <cgb@datanalytics.com>
2016-08-07 02:31:33 +0200

    Old changes

--- R/read.fwf.microdata.R
+++ R/read.fwf.microdata.R
@@ -2,36 +2,57 @@
 # cjgb
 # 20120305
 # Reads a generic microdata file
+# Changes:
+#   20160806: changed workhorse to readr::read_fwf
 ###################################################################
 
-read.fwf.microdata <- function( file, file.mdat.1, file.mdat.2, fileEncoding = "UTF-8" ){
+read.fwf.microdata <- function(file, file.mdat.1, file.mdat.2, fileEncoding = "UTF-8"){
+  
+  ## read metadata
+  foo <- function(x) read.table(x, header = T, sep = "\t", 
+                                fileEncoding = fileEncoding,
+                                stringsAsFactors = FALSE)
+  mdat.1 <- foo(file.mdat.1)
+  mdat.2 <- foo(file.mdat.2)
 
 	## read fixed file using mdat1 metadata file
-
-	mdat.1 <- read.table( file.mdat.1, header = T, sep = "\t", fileEncoding = fileEncoding )
-	dat <- read.fwf( file, mdat.1$width, header = F, col.names = mdat.1$var, fileEncoding = fileEncoding )
-
-	rm( mdat.1 ); gc()
+	dat <- read_fwf(file, fwf_widths(mdat.1$width, col_names = mdat.1$var))
 
 	# Replaces keys in raw data by actual column values
+	assign.labels <-  function(v, var.name, mdat){
+	  
+	  print(var.name)
+	  
+		tmp <- mdat[mdat$var == var.name,]
 
-	mdat.2 <- read.table( file.mdat.2, header = T, sep = "\t", fileEncoding = fileEncoding )
-
-	assign.labels <-  function( v, var.name, mdat ){
-		tmp <- mdat[ mdat$var == var.name, ]
-
-		if( nrow( tmp ) == 1 ){
-			if( ! is.na( tmp$nulo ) && any( v == tmp$nulo, na.rm = T ) )
-				v[ v == tmp$nulo ] <- NA
-			if( tmp$tipo == "HHMM" )
-				v <- v %/% 100 + ( v %% 100 ) / 60
-			return( v )
+		# special cases: numeric, etc.
+		if (nrow(tmp) == 1){
+			if (!is.na(tmp$nulo) && any(v == tmp$nulo, na.rm = T))
+				v[v == tmp$nulo] <- NA
+			
+			if (tmp$tipo == "N")
+			  return(as.numeric(v))
+			
+			if(tmp$tipo == "HHMM"){
+			  v <- as.numeric(v)
+				return(v %/% 100 + ( v %% 100 ) / 60)
+			}
+			return(v)
 		}
-
-		indices <- match( v, tmp$llave )
-		return( as.character( tmp$valor )[ indices ] )
+		
+		v <- factor(v)
+		
+		# Check whether keys are numbers (usual case)
+		# Then, format codes (maybe like "07") into codes such like "7"
+		if (!any(is.na(as.numeric(levels(v))))){
+		  levels(v) <- as.character(as.numeric(levels(v)))
+		}
+		
+		# replace codes by descriptions
+		levels(v) <- tmp$valor[match(levels(v), tmp$llave)]
+		as.character(v)
 	}
 
-	as.data.frame( sapply( names( dat ), function( x ) assign.labels( dat[[x]], x, mdat.2 ), simplify = F ) )
+	as.data.frame(sapply(names(dat), function(x) assign.labels(dat[[x]], x, mdat.2), simplify = F))
 }
 
