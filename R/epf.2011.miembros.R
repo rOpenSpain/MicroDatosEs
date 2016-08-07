b32c1e48665a1e9371b362443e897ba3f2b520da
Carlos J. Gil Bellosta <cgb@datanalytics.com>
2016-08-07 05:42:59 +0200

    Deleted old metadata files and replaced memisc by readr

--- R/epf.2011.miembros.R
+++ R/epf.2011.miembros.R
@@ -4,23 +4,10 @@
 # Reads the members EPF microdata in its 2011 version
 ###################################################################
 
-epf.2011.miembros <- function(file) 
-{       
-        file.column <- create.spss.column(system.file("metadata", 
-                                                      "epf_2011_miembros_mdat1.txt", package = "MicroDatosEs"), system.file("metadata", 
-                                                                                                                     "epf_2011_miembros_mdat2.txt", package = "MicroDatosEs"), fileEncoding = "UTF-8")
-        
-        
-        file.var <- create.spss.var(system.file("metadata", "epf_2011_miembros_mdat1.txt", 
-                                                package = "MicroDatosEs"), fileEncoding = "UTF-8")
-        file.vals <- create.spss.vals(system.file("metadata", "epf_2011_miembros_mdat2.txt", 
-                                                  package = "MicroDatosEs"), fileEncoding = "UTF-8")
-        file.missing <- system.file("metadata", "epf_2011_miembros_mdat3.txt", 
-                                    package = "MicroDatosEs")
-        x <- spss.fixed.file(file = file, columns.file = file.column, 
-                             varlab.file = file.var, missval.file = file.missing, 
-                             codes.file = file.vals)
-        
-        as.data.set(x)
+epf.2011.miembros <- function(file){       
+  read.fwf.microdata(file, 
+                     system.file( "metadata", "epf_2011_miembros_mdat1.txt", package = "MicroDatosEs" ),
+                     system.file( "metadata", "epf_2011_miembros_mdat2.txt", package = "MicroDatosEs" ),
+                     fileEncoding = "UTF-8")
 }
 
--- R/read.fwf.microdata.R
+++ R/read.fwf.microdata.R
@@ -19,14 +19,11 @@ read.fwf.microdata <- function(file, file.mdat.1, file.mdat.2, fileEncoding = "U
 	dat <- read_fwf(file, fwf_widths(mdat.1$width, col_names = mdat.1$var))
 
 	# Replaces keys in raw data by actual column values
-	assign.labels <-  function(v, var.name, mdat){
-	  
-	  print(var.name)
-	  
-		tmp <- mdat[mdat$var == var.name,]
+	assign.labels <-  function(v, var.name){
+		tmp <- mdat.2[mdat.2$var == var.name,]
 
 		# special cases: numeric, etc.
-		if (nrow(tmp) == 1){
+		if (nrow(tmp) == 1 && tmp$tipo != "D"){
 			if (!is.na(tmp$nulo) && any(v == tmp$nulo, na.rm = T))
 				v[v == tmp$nulo] <- NA
 			
@@ -40,19 +37,26 @@ read.fwf.microdata <- function(file, file.mdat.1, file.mdat.2, fileEncoding = "U
 			return(v)
 		}
 		
-		v <- factor(v)
-		
 		# Check whether keys are numbers (usual case)
 		# Then, format codes (maybe like "07") into codes such like "7"
+		v <- factor(v)
 		if (!any(is.na(as.numeric(levels(v))))){
 		  levels(v) <- as.character(as.numeric(levels(v)))
 		}
 		
-		# replace codes by descriptions
-		levels(v) <- tmp$valor[match(levels(v), tmp$llave)]
+		# replace codes by descriptions (where available)
+		# if no match, leave underlying code: it may correspond to municipality, etc.
+		# indeed, in some cases (e.g., municipality) codes correspond to municipalities
+		# and extra codes mark small size municipalities
+		levels(v) <- sapply(levels(v), function(x){ 
+		  if (length(where <- which(tmp$llave == x)) > 0)
+		    tmp$valor[where]
+		  else x
+		})
+		                    
 		as.character(v)
 	}
 
-	as.data.frame(sapply(names(dat), function(x) assign.labels(dat[[x]], x, mdat.2), simplify = F))
+	as.data.frame(lapply(names(dat), function(x) assign.labels(dat[[x]], x)))
 }
 
