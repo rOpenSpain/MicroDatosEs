10e6c62da18acd6e5023ebac6acd35e3e4d70db8
Gil Bellosta, Carlos <cgb@datanalytics.com>
2017-04-27 17:40:13 +0200

    Added support for the padron (and modified required dependencies)

---
+++ R/padron2016.R
@@ -0,0 +1,26 @@
+###################################################################
+# cjgb
+# 20170427
+# Reads the "padron" microdata in its 2016 (and probably others) version
+###################################################################
+
+padron2016 <- function(file){
+  
+  mdat.1 <- read.table(system.file("metadata", "padron_2016_mdat1.txt", package = "MicroDatosEs"), 
+                       header = T, sep = "\t", 
+                       fileEncoding = "UTF-8",
+                       stringsAsFactors = FALSE)
+  
+  suppressMessages(
+    dat <- read_fwf(file, fwf_widths(mdat.1$width, col_names = mdat.1$var))
+  )
+  
+  dat$CMUN  <- paste0(dat$CPRO, dat$CMUN)
+  dat$CMUNN <- paste0(dat$CPRON, dat$CMUNN)
+  
+  read.fwf.microdata(dat, 
+                     system.file("metadata", "padron_2016_mdat1.txt", package = "MicroDatosEs"),
+                     system.file("metadata", "padron_2016_mdat2.txt", package = "MicroDatosEs"),
+                     fileEncoding = "UTF-8")
+}
+
