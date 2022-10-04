46e9583ba74ebb79c8c36a2d7bba1c264232efc7
Carlos J. Gil Bellosta <cgb@circiter.es>
2022-10-04 23:59:07 +0200

    Various changes

--- R/read.fwf.microdata.R
+++ R/read.fwf.microdata.R
@@ -8,9 +8,9 @@
 ###################################################################
 
 read.fwf.microdata <- function(file, file.mdat.1, file.mdat.2, fileEncoding = "UTF-8"){
-  
+
   ## read metadata
-  foo <- function(x) read.table(x, header = T, sep = "\t", 
+  foo <- function(x) read.table(x, header = T, sep = "\t",
                                 fileEncoding = fileEncoding,
                                 stringsAsFactors = FALSE)
   mdat.1 <- foo(file.mdat.1)
@@ -26,29 +26,37 @@ read.fwf.microdata <- function(file, file.mdat.1, file.mdat.2, fileEncoding = "U
   } else {
     dat <- file
   }
-	
+
 	# Replaces keys in raw data by actual column values
 	assign.labels <-  function(v, metadat){
-		
+
 		# happens!
 	  if (all(is.na(v)))
 	    return(v)
-	  
+
+    tipo <- unique(metadat$tipo)
+
+    if (length(tipo) > 1)
+      stop("A column cannot have more than a single type: check metadata file for inconsistencies.")
+
+    if (!tipo %in% c("D", "N", "HHMM"))
+      stop("Column types can only be D (key-value), N (numeric) or HHMM (hour-minute)."
+
 	  # special cases: numeric, etc.
-		if (nrow(metadat) == 1 && metadat$tipo != "D"){
+		if (nrow(metadat) == 1 && tipo != "D"){
 			if (!is.na(metadat$nulo) && any(v == metadat$nulo, na.rm = T))
 				v[v == metadat$nulo] <- NA
-			
-			if (metadat$tipo == "N")
+
+			if (tipo == "N")
 			  return(as.numeric(v))
-			
-			if(metadat$tipo == "HHMM"){
+
+			if(tipo == "HHMM"){
 			  v <- as.numeric(v)
 				return(v %/% 100 + ( v %% 100 ) / 60)
 			}
 			return(v)
 		}
-		
+
 		# Check whether keys are numbers (usual case)
 	  # Mind the double negation!
 		# Then, format codes (maybe like "07") into codes such like "7"
@@ -56,23 +64,22 @@ read.fwf.microdata <- function(file, file.mdat.1, file.mdat.2, fileEncoding = "U
 		if (!grepl("[^0-9]", paste(levels(v), collapse = ""))){
 		  levels(v) <- as.character(as.numeric(levels(v)))
 		}
-		
+
 		# replace codes by descriptions (where available)
 		# if no match, leave underlying code: it may correspond to municipality, etc.
 		# indeed, in some cases (e.g., municipality) codes correspond to municipalities
 		# and extra codes mark small size municipalities
-		levels(v) <- sapply(levels(v), function(x){ 
+		levels(v) <- sapply(levels(v), function(x){
 		  if (length(where <- which(metadat$llave == x)) > 0)
 		    metadat$valor[where]
 		  else x
 		})
-		                    
+
 		as.character(v)
 	}
-	
+
 	for(colname in colnames(dat))
 	  dat[[colname]] <- assign.labels(dat[[colname]], mdat.2[mdat.2$var == colname,])
-	
+
 	dat
 }
-
